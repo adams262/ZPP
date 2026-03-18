25e09a7d0e2f0924646dd7577e6de2a4afd62e78
Kozubcio <mateuszkozubekosobisty@gmail.com>
2026-03-18 20:23:47 +0100

    Projekt_v1
    Utworzono szkielet aplikacji.

---
+++ PZPP_Grupa5/MauiProgram.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace PZPP_Grupa5
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+            // [[[ Dependency Injection klienta YouTube ]]]
+            builder.Services.AddSingleton<YoutubeExplode.YoutubeClient>();
+
+            // [[[ Rejestracja widoków i logiki UI ]]]
+            builder.Services.AddTransient<PZPP_Grupa5.ViewModels.MainViewModel>();
+            builder.Services.AddTransient<PZPP_Grupa5.Views.MainPage>();
+
+            // [[[ Rejestracja serwisów ]]]
+            builder.Services.AddSingleton<PZPP_Grupa5.Services.IYouTubeService, PZPP_Grupa5.Services.YouTubeService>();
+            builder.Services.AddSingleton<PZPP_Grupa5.Services.IGeminiService, PZPP_Grupa5.Services.GeminiService>();
+
+            return builder.Build();
+        }
+    }
+}
